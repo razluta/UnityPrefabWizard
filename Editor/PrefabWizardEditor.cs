ac90619ff461627e629ed9ff470fb8d2bddec417
Razvan Luta <razvan.luta@gmail.com>
2020-07-20 23:28:54 -0700

    A default window is now being build.

---
+++ Editor/PrefabWizardEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityPrefabWizard.Editor
+{
+    public class PrefabWizardEditor : EditorWindow
+    {
+        // private string _version = "v.0.0.1.20200710";
+        
+        private const int WindowWidth = 200;
+        private const int WindowHeight = 350;
+
+        private VisualElement _root;
+        private VisualTreeAsset _contents;
+        
+        [MenuItem("Art Tools/Launch Prefab Wizard")]                                                                                     
+        public static void ShowWindow()                                                                                                      
+        {                                                                                                                                    
+            // Opens the window, otherwise focuses it if it’s already open.                                                                  
+            var window = GetWindow<PrefabWizardEditor>();
+
+            // Adds a title to the window.                                                                                                   
+            window.titleContent = new GUIContent("Prefab Wizard");                                                                
+                                                                                                                                             
+            // Sets a minimum size to the window.                                                                                            
+            window.minSize = new Vector2(WindowWidth, WindowHeight);
+        }                                                                                                                                    
+                                                                                                                                             
+        private void OnEnable()                                                                                                              
+        {                                                                                                                                    
+            // Reference to the root of the window.                                                                                          
+            _root = rootVisualElement;
+            
+            // Instantiate contents
+            _contents = Resources.Load<VisualTreeAsset>("CS_PrefabWizard");
+            _contents.CloneTree(_root);
+        }
+    }                                                                                                                                        
+}
