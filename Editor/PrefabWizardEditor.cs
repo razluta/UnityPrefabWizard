e13b00122fe758ce136c20bdfea3e137583eff8f
Razvan Luta <razvan.luta@gmail.com>
2020-07-21 00:22:55 -0700

    Adjusted the size of the editor window.

--- Editor/PrefabWizardEditor.cs
+++ Editor/PrefabWizardEditor.cs
@@ -8,8 +8,8 @@ namespace UnityPrefabWizard.Editor
     {
         // private string _version = "v.0.0.1.20200710";
         
-        private const int WindowWidth = 200;
-        private const int WindowHeight = 350;
+        private const int WindowWidth = 400;
+        private const int WindowHeight = 600;
 
         private VisualElement _root;
         private VisualTreeAsset _contents;
