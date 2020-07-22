3b3e60f6515ef5a8dd9c0aef680fe13376476183
Razvan Luta <razvan.luta@gmail.com>
2020-07-21 18:33:24 -0700

    Modified prefab wizard so that rules are instantiated when the user hits add.

--- Editor/PrefabWizardEditor.cs
+++ Editor/PrefabWizardEditor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityPrefabWizard.AssetUtilities;
 
 namespace UnityPrefabWizard.Editor
 {
@@ -13,7 +14,14 @@ namespace UnityPrefabWizard.Editor
 
         private VisualElement _root;
         private VisualTreeAsset _contents;
-        
+
+        private ListView _rulesListView;
+        private VisualTreeAsset _singleRuleVisualTree;
+        private VisualElement _singleRuleVisualElement;
+
+        private int _ruleCount;
+        private Button _addRule;
+
         [MenuItem("Art Tools/Launch Prefab Wizard")]                                                                                     
         public static void ShowWindow()                                                                                                      
         {                                                                                                                                    
@@ -27,14 +35,57 @@ namespace UnityPrefabWizard.Editor
             window.minSize = new Vector2(WindowWidth, WindowHeight);
         }                                                                                                                                    
                                                                                                                                              
-        private void OnEnable()                                                                                                              
-        {                                                                                                                                    
+        private void OnEnable()
+        {
+            _ruleCount = 0;
+            
             // Reference to the root of the window.                                                                                          
             _root = rootVisualElement;
             
             // Instantiate contents
             _contents = Resources.Load<VisualTreeAsset>("CS_PrefabWizard");
             _contents.CloneTree(_root);
+
+            _rulesListView = _root.Q<ListView>("LV_RulesList");
+            
+            // Single Rule Reference
+            _singleRuleVisualTree = Resources.Load<VisualTreeAsset>("CS_SingleRule");
+
+            // Load Rules Button
+            
+            // Save Rules Button
+            
+            // Add Rule Button
+            _addRule = _root.Q<Button>("BT_AddRule");
+            _addRule.clickable.clicked += AddNewRule;
+        }
+
+        private void AddNewRule()
+        {
+            _singleRuleVisualTree.CloneTree(
+                _rulesListView.hierarchy.ElementAt(0
+                ).hierarchy.ElementAt(0
+                ).hierarchy.ElementAt(0));
+            var randomColor = GeneralUtilities.GetRandomColor();
+            var newRuleVisualElement = _root.Q<VisualElement>("VE_SingleRule");
+            
+            newRuleVisualElement.name += _ruleCount.ToString();
+            newRuleVisualElement.style.borderTopColor = new StyleColor(randomColor);
+            newRuleVisualElement.style.borderRightColor = new StyleColor(randomColor);
+            newRuleVisualElement.style.borderBottomColor = new StyleColor(randomColor);
+            newRuleVisualElement.style.borderLeftColor = new StyleColor(randomColor);
+
+            var removeButton = _root.Q<Button>("BT_Remove");
+            removeButton.name += _ruleCount.ToString();
+            removeButton.clickable.clicked += () => RemoveRule(newRuleVisualElement);
+            removeButton.style.backgroundColor = randomColor;
+            
+            _ruleCount++;
+        }
+
+        private void RemoveRule(VisualElement singleRule)
+        {
+            _rulesListView.Remove(singleRule);
         }
     }                                                                                                                                        
 }
