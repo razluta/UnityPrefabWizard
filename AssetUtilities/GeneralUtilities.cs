fce858f2287c27774e01d120d7d5f5bd7f3b19ea
Razvan Luta <razvan.luta@gmail.com>
2020-07-21 18:32:47 -0700

    Added a general utility for getting a random color.

---
+++ AssetUtilities/GeneralUtilities.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace UnityPrefabWizard.AssetUtilities
+{
+    public static class GeneralUtilities
+    {
+        public static Color GetRandomColor()
+        {
+            return new Color(
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f));
+        }
+    }
+}
