377e233735ce26d1b46b0e7c4c9a75bbe89c9884
Razvan Luta <razvan.luta@gmail.com>
2020-07-25 23:15:19 -0700

    Added a prefab wizard class for interacting with the rules class

---
+++ PrefabWizard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityPrefabWizard.SystemUtilities;
+
+namespace UnityPrefabWizard
+{
+    public static class PrefabWizard
+    {
+        public static List<Rule> GetRules(string path=null)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                return GetDefaultRules();
+            }
+            
+            var rules = JsonUtilities.GetData(path);
+            return rules;
+        }
+        
+        public static void SetRules(List<Rule> rules, string path)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+            
+            JsonUtilities.SetData(rules, path); 
+        }
+
+        private static List<Rule> GetDefaultRules()
+        {
+            var rules = new List<Rule>();
+            rules.Add(
+                new Rule()
+                {
+                    
+                });
+
+            return rules;
+        }
+    }
+}
