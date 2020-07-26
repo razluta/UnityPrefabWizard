9ddf58a9c213999742c956f4fc694ffc4066580e
Razvan Luta <razvan.luta@gmail.com>
2020-07-25 23:14:45 -0700

    Added my own json utilities.

---
+++ SystemUtilities/JsonUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace UnityPrefabWizard.SystemUtilities
+{
+    public static class JsonUtilities
+    {
+        public static List<Rule> GetData(string path)
+        {
+            var file = File.OpenText(path);
+            var serializers = new JsonSerializer();
+            var rules = (List<Rule>) serializers.Deserialize(file, typeof(List<Rule>));
+            return rules;
+        }
+        
+        public static void SetData(List<Rule> rules, string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(rules));
+        }
+    }
+}
